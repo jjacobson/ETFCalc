9c4506ce2df6f00e17d44189b7adc339bc06a967
Jeremiah Jacobson <j.jacobson966@gmail.com>
2018-08-20 22:35:29 -0800

    No need for webscraper to be a class

--- etfcalc/__init__.py
+++ etfcalc/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request
-from util.webscraper import WebScraper
+from util import webscraper
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    scraper = WebScraper()
-    data = scraper.scrape_ticker('QQQ')
+    data = webscraper.scrape_ticker('QQQ')
     return str(data[97])
 
 if __name__ == '__main__':
--- etfcalc/util/portfolio.py
+++ etfcalc/util/portfolio.py
@@ -1,6 +1,19 @@
 class Portfolio(object):
 
-    def __init__(self, name, ticker, weight=1):
-        self.name = name
-        self.ticker = ticker
-        self.weight = weight
+    def __init__(self):
+        self.holdings = {}
+
+    def set_amount(self, ticker, amount):
+        self.holdings[ticker] = amount
+
+    def get_amount(self, ticker):
+        return self.holdings[ticker]
+
+    def remove_holding(self, ticker):
+        del self.holdings[holding]
+
+    def clear_holdings(self):
+        self.holdings.clear()
+    
+
+
