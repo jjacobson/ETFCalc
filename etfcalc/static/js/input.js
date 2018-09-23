37df6c368ff1a831aea1cfc4a4867e4dbe461bb8
Jeremiah Jacobson <j.jacobson966@gmail.com>
2018-09-23 04:10:35 -0800

    add/remove row buttons

--- etfcalc/static/js/input.js
+++ etfcalc/static/js/input.js
@@ -1,14 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-    //Wait to add event listeners until the DOM is fully loaded. This is needed
-    // when wanting to access elements that are later in the HTML than the <script>.
-    for (var i = 0; i < 5; i++) {
+document.addEventListener('DOMContentLoaded', function () {
+    for (var i = 0; i < 3; i++) {
         add_row();
     }
 });
 
 function add_row() {
-    var row = document.getElementsByTagName("template")[0];
+    let row = document.getElementsByTagName("template")[0];
     let table = document.getElementById('holding-table');
     let clone = row.content.cloneNode(true);
     table.appendChild(clone);
+}
+
+function remove_row(el) {
+    let tr = el.parentElement.parentElement;
+    tr.parentElement.removeChild(tr);
 }
