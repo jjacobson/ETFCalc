9a0f41629c0d90ce39fbfd23c0b2448b9455bf9f
Jeremiah Jacobson <j.jacobson966@gmail.com>
2019-01-03 04:16:08 -0900

    Use faster data source when available

--- etfcalc/util/webscraper.py
+++ etfcalc/util/webscraper.py
@@ -1,11 +1,15 @@
-import requests, json, requests_cache, time, logging
+import requests
+import json
+import requests_cache
+import time
+import logging
 from datetime import timedelta
 from pyquery import PyQuery
 from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
 from .holding import Holding
 
 symbols = get_nasdaq_symbols()
-expire_after = timedelta(days=7)
+expire_after = timedelta(days=5)
 requests_cache.install_cache('cache_data', expire_after=expire_after)
 
 # Scrape name and holdings if any for a given ticker
@@ -32,11 +36,45 @@ def get_data(ticker):
         logging.info('Failed to get data for ticker ', ticker)
     return data
 
+
 def _is_etf(data):
     return data.loc['ETF']
 
+
 def _get_etf_data(ticker, data, holdings):
     response = _get_etf_page(ticker)
+    if not _valid_request(response):
+        logging.warning('Failed to get holdings for ticker',
+                        ticker, response.status_code)
+        return
+
+    page_content = response.content
+    pq = PyQuery(page_content)
+    table = pq.find('#etfs-that-own')
+
+    # use secondary data source if none available
+    if not table:
+        _get_etf_data_backup(ticker, data, holdings)
+        return
+
+    for row in table('tbody tr').items():
+        columns = list(row('td').items())
+        ticker = columns[0].children("a").text()
+        holding_data = get_data(ticker)
+        if holding_data is None:
+            # fall back to getting name from scraped data
+            name = columns[1].children("a").text()
+        else:
+            # make use of official nasdaq data if available
+            name = holding_data.loc['Security Name']
+
+        weight = columns[2].text()
+        weight = weight[:-1]
+        holdings.append(Holding(name, ticker, weight))
+
+
+def _get_etf_data_backup(ticker, data, holdings):
+    response = _get_etf_page_backup(ticker)
     if not _valid_request(response):
         logging.warning('Failed to get holdings for ticker ', ticker)
         return
@@ -45,31 +83,39 @@ def _get_etf_data(ticker, data, holdings):
     title = data.loc['Security Name']
 
     url = _get_holdings_url(page_content)
-    holdings_json = _make_request(url + str(0)).json()
+    holdings_json = _make_request(url + str(0), throttle=0.5).json()
     rows = holdings_json['total']
     # etfdb limits us to 15 tickers per page
     for i in range(0, rows, 15):
         for entry in holdings_json['rows']:
             holding = _get_etf_holding(entry)
             holdings.append(holding)
-        holdings_json = _make_request(url + str(i + 15)).json()
+        holdings_json = _make_request(url + str(i + 15), throttle=0.5).json()
+
 
 def _get_stock_data(ticker, data, holdings):
     title = data.loc['Security Name']
     holding = Holding(title, ticker)
     holdings.append(holding)
 
-def _get_etf_page(ticker):
-    url = 'https://etfdb.com/etf/' + ticker + '/'
-    return _make_request(url, False)
 
-def _make_request(url, redirects = True):
+def _get_etf_page(ticker):
+    url = 'https://etfdailynews.com/etf/' + ticker + '/'
+    return _make_request(url, redirects=False)
+
+
+def _get_etf_page_backup(ticker):
+    url = 'https://etfdb.com/etf/' + ticker + '/'
+    return _make_request(url, redirects=False)
+
+
+def _make_request(url, redirects=True, throttle=0.0):
     response = None
     try:
-        response = requests.get(url, hooks={'response': _throttle_hook(0.5)}, allow_redirects=redirects, timeout=1)
+        response = requests.get(url, hooks={'response': _throttle_hook(
+            throttle)}, allow_redirects=redirects, timeout=3)
     except requests.exceptions.RequestException as e:
         raise ValueError('ETF scraping exception') from e
-
     return response
 
 # returns response hook function which sleeps for
@@ -85,12 +131,14 @@ def _throttle_hook(timeout):
 def _valid_request(response):
     return response.status_code == requests.codes.ok
 
+
 def _get_holdings_url(content):
     pq = PyQuery(content)
-    url = 'http://etfdb.com/'
+    url = 'https://etfdb.com/'
     sort = '&sort=weight&order=desc&limit=15&offset='
     url += pq("table[data-hash='etf-holdings']").attr('data-url') + sort
     return url
+
 
 def _get_etf_holding(entry):
     name = ticker = ''
